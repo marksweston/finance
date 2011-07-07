a1fdcc659dbc3d82effe758caa597aa092f4204a
William Kranec <wkranec@gmail.com>
2011-07-07 22:11:18 +0200

    Intoduced shoulda to better structure and document unit tests.

--- test/test_amortization.rb
+++ test/test_amortization.rb
@@ -1,156 +1,152 @@
-require 'rubygems'
 require 'finance'
 require 'flt/d'
+require 'shoulda'
 require 'test/unit'
 
-class TestBasicAmortization < Test::Unit::TestCase
-  def interest_in_period(principal, rate, payment, period)
+# @see http://tinyurl.com/6zroqvd for detailed calculations for the
+# examples in these unit tests.
+class TestAmortization < Test::Unit::TestCase
+  def ipmt(principal, rate, payment, period)
     -(-rate*principal*(1+rate)**(period-1) - payment*((1+rate)**(period-1)-1)).round(2)
   end
 
-  def setup
-    @rate = Rate.new(0.0375, :apr, :duration => 30.years)
-    @principal = D(200000)
-    @amortization = Amortization.new(@principal, @rate)
-  end
-
-  def test_balance
-    assert @amortization.balance.zero?
-  end
-
-  def test_duration
-    assert_equal 360, @amortization.duration
-  end
-
-  def test_interest
-    0.upto 359 do |period|
-      assert_equal @amortization.interest[period], interest_in_period(@principal, @rate.monthly, @amortization.payment, period+1)
+  context "a fixed-rate amortization of 200000 at 3.75% over 30 years" do
+    setup do
+      @rate = Rate.new(0.0375, :apr, :duration => 30.years)
+      @principal = D(200000)
+      @std = Amortization.new(@principal, @rate)
     end
-  end
 
-  def test_interest_sum
-    assert_equal D('133443.53'), @amortization.interest.sum
-  end
-
-  def test_payment
-    assert_equal D('-926.23'), @amortization.payment
-  end
-
-  def test_payments
-    payments = [ D('-926.23') ] * @rate.duration
-    # Account for rounding errors in last payment.
-    payments[-1] = D('-926.96')
-    assert_equal payments, @amortization.payments
-  end
-
-  def test_payments_sum
-    assert_equal D('-333443.53'), @amortization.payments.sum
-  end
-
-  def test_principal
-    assert_equal @principal, @amortization.principal
-  end
-
-  def test_sum
-    assert_equal D(0), @amortization.payments.sum + @amortization.interest.sum + @amortization.principal
-  end
-end
-
-class TestAdjustableAmortization < Test::Unit::TestCase
-  def setup
-    @rates = []
-    0.upto 9 do |adj|
-      @rates << Rate.new(0.0375 + (D('0.01') * adj), :apr, :duration => 3.years)
+    should "have a principal of $200,000" do
+      assert_equal @principal, @std.principal
     end
-    @principal = D(200000)
-    @amortization = Amortization.new(@principal, *@rates)
-  end
 
-  def test_balance
-    assert @amortization.balance.zero?
-  end
+    should "have a final balance of zero" do
+      assert @std.balance.zero?
+    end
 
-  def test_duration
-    assert_equal 360, @amortization.duration
-  end
+    should "have a duration of 360 months" do
+      assert_equal 360, @std.duration
+    end
 
-  def test_interest_sum
-    assert_equal D('277505.92'), @amortization.interest.sum
-  end
+    should "have a monthly payment of $926.23" do
+      assert_equal D('-926.23'), @std.payment
+    end
 
-  def test_payment
-    assert_nil @amortization.payment
-  end
+    should "have a final payment of $926.96 (due to rounding)" do
+      assert_equal D('-926.96'), @std.payments[-1]
+    end
 
-  def test_payments
-    values = %w{926.23 1033.73 1137.32 1235.39 1326.30 1408.27 1479.28 1537.03 1578.84 1601.66 1601.78}
-    values.collect!{ |v| -D(v) }
-    
-    payments = []
-    values[0,9].each do |v|
-      36.times do
-        payments << v
+    should "have total payments of $333,443.53" do
+      assert_equal D('-333443.53'), @std.payments.sum
+    end
+
+    should "have interest charges which agree with the standard formula" do
+      0.upto 359 do |period|
+        assert_equal @std.interest[period], ipmt(@principal, @rate.monthly, @std.payment, period+1)
       end
     end
 
-    35.times { payments << values[9] }
-    payments << values[10]
-
-    payments.each_with_index do |payment, index|
-      assert_equal payment, @amortization.payments[index]
+    should "have total interest charges of $133,433.33" do
+      assert_equal D('133443.53'), @std.interest.sum
     end
   end
 
-  def test_payment_sum
-    assert_equal D('-477505.92'), @amortization.payments.sum
+  context "an adjustable rate amortization of 200000 starting at 3.75% and increasing by 1% every 3 years" do
+    setup do
+      @rates = []
+      0.upto 9 do |adj|
+        @rates << Rate.new(0.0375 + (D('0.01') * adj), :apr, :duration => 3.years)
+      end
+      @principal = D(200000)
+      @arm = Amortization.new(@principal, *@rates)
+    end
+
+    should "have a principal of $200,000" do
+      assert_equal @principal, @arm.principal
+    end
+
+    should "have a final balance of zero" do
+      assert @arm.balance.zero?
+    end
+
+    should "have a duration of 360 months" do
+      assert_equal 360, @arm.duration
+    end
+
+    should "not have a fixed monthly payment (since it changes)" do
+      assert_nil @arm.payment
+    end
+
+    should "have payments which increase every three years" do
+      values = %w{926.23 1033.73 1137.32 1235.39 1326.30 1408.27 1479.28 1537.03 1578.84 1601.66 }
+      values.collect!{ |v| -D(v) }
+      
+      payments = []
+      values[0,9].each do |v|
+        36.times do
+          payments << v
+        end
+      end
+
+      35.times { payments << values[9] }
+
+      payments[0..-2].each_with_index do |payment, index|
+        assert_equal payment, @arm.payments[index]
+      end
+    end
+
+    should "have a final payment of $1601.78 (due to rounding)" do
+      assert_equal D('-1601.78'), @arm.payments[-1]
+    end
+
+    should "have total payments of $47,505.92" do
+      assert_equal D('-477505.92'), @arm.payments.sum
+    end
+
+    should "have total interest charges of $277,505.92" do
+      assert_equal D('277505.92'), @arm.interest.sum
+    end
   end
 
-  def test_principal
-    assert_equal @principal, @amortization.principal
-  end
+  context "a fixed-rate amortization of 200000 at 3.75% over 30 years, where an additional 100 is paid each month" do
+    setup do
+      @rate = Rate.new(0.0375, :apr, :duration => 30.years)
+      @principal = D(200000)
+      @exp = Amortization.new(@principal, @rate){ |period| period.payment - 100 }
+    end
 
-  def test_sum
-    assert_equal D(0), @amortization.payments.sum + @amortization.interest.sum + @amortization.principal
-  end
-end
+    should "have a principal of $200,000" do
+      assert_equal @principal, @exp.principal
+    end
 
-class TestExtraPaymentAmortization < Test::Unit::TestCase
-  def setup
-    @rate = Rate.new(0.0375, :apr, :duration => 30.years)
-    @principal = D(200000)
-    @amortization = Amortization.new(@principal, @rate){ |period| period.payment - 100 }
-  end
+    should "have a final balance of zero" do
+      assert @exp.balance.zero?
+    end
 
-  def test_additional_payments_sum
-    assert_equal D('-30084.86'), @amortization.additional_payments.sum
-  end
-  
-  def test_balance
-    assert @amortization.balance.zero?
-  end
+    should "have a duration of 301 months" do
+      assert_equal 301, @exp.duration
+    end
 
-  def test_duration
-    assert_equal 301, @amortization.duration
-  end
+    should "have a monthly payment of $1026.23" do
+      assert_equal D('-1026.23'), @exp.payment
+    end
 
-  def test_interest_sum
-    assert_equal D('108880.09'), @amortization.interest.sum
-  end
+    should "have a final payment of $1011.09" do
+      assert_equal D('-1011.09'), @exp.payments[-1]
+    end
 
-  def test_payment
-    assert_equal D('-1026.23'), @amortization.payment
-  end
+    should "have total payments of $308,880.09" do
+      assert_equal D('-308880.09'), @exp.payments.sum
+    end
 
-  def test_payment_sum
-    assert_equal D('-308880.09'), @amortization.payments.sum
-  end
+    should "have total additional payments of $30,084.86" do
+      assert_equal D('-30084.86'), @exp.additional_payments.sum
+    end
 
-  def test_principal
-    assert_equal @principal, @amortization.principal
-  end
-
-  def test_sum
-    assert_equal D(0), @amortization.payments.sum + @amortization.interest.sum + @amortization.principal
+    should "have total interest charges of $108880.09" do
+      assert_equal D('108880.09'), @exp.interest.sum
+    end
   end
 end
 
--- test/test_rates.rb
+++ test/test_rates.rb
@@ -1,31 +1,77 @@
-require 'rubygems'
 require 'finance'
+require 'rubygems'
+require 'flt'
 require 'flt/d'
+require 'shoulda'
 require 'test/unit'
 
 class TestRates < Test::Unit::TestCase
-  def test_apr
-    rate = Rate.new(0.0375, :apr)
-    assert_equal D('0.03687'), rate.nominal.round(5)
-  end
+  context "an interest rate" do
+    context "can compound with different periods" do
+      should "compound monthly by default" do
+        rate = Rate.new(0.15, :nominal)
+        assert_equal D('0.16075'), rate.effective.round(5)
+      end
 
-  def test_duration
-    rate = Rate.new(0.0375, :effective, :duration => 30.years)
-    assert_equal 360, rate.duration
-  end
+      should "compound annually" do
+        rate = Rate.new(0.15, :nominal, :compounds => :annually)
+        assert_equal D('0.15'), rate.effective
+      end
+
+      should "compound continuously" do
+        rate = Rate.new(0.15, :nominal, :compounds => :continuously)
+        assert_equal D('0.16183'), rate.effective.round(5)
+      end
+
+      should "compound daily" do
+        rate = Rate.new(0.15, :nominal, :compounds => :daily)
+        assert_equal D('0.16180'), rate.effective.round(5)
+      end
+
+      should "compound quarterly" do
+        rate = Rate.new(0.15, :nominal, :compounds => :quarterly)
+        assert_equal D('0.15865'), rate.effective.round(5)
+      end
+
+      should "compound semiannually" do
+        rate = Rate.new(0.15, :nominal, :compounds => :semiannually)
+        assert_equal D('0.15563'), rate.effective.round(5)
+      end
+
+      should "accept a numerical value as the compounding frequency per year" do
+        rate = Rate.new(0.15, :nominal, :compounds => 7)
+        assert_equal D('0.15999'), rate.effective.round(5)
+      end
+
+      should "raise an exception if an unknown string is given" do
+        assert_raises(ArgumentError){ Rate.new(0.15, :nominal, :compounds => :quickly) }
+      end
+    end
+
+    should "accept a duration if given" do
+      rate = Rate.new(0.0375, :effective, :duration => 30.years)
+      assert_equal 360, rate.duration
+    end
+
+    should "be comparable to other interest rates" do
+      r1 = Rate.new(0.15, :nominal)
+      r2 = Rate.new(0.16, :nominal)
+      assert_equal 1, r2 <=> r1
+      assert_equal -1, r1 <=> r2
+    end
     
-  def test_effective
-    rate = Rate.new(0.03687, :nominal)
-    assert_equal D('0.0375'), rate.effective.round(4)
-  end
+    should "convert to a monthly value" do
+      rate = Rate.new(0.0375, :effective)
+      assert_equal D('0.003125'), rate.monthly
+    end
 
-  def test_monthly
-    rate = Rate.new(0.0375, :effective)
-    assert_equal D('0.003125'), rate.monthly
-  end
+    should "convert effective interest rates to nominal" do
+      assert_equal D('0.03687'), Rate.to_nominal(D('0.0375'), 12).round(5)
+      assert_equal D('0.03681'), Rate.to_nominal(D('0.0375'), Flt::DecNum.infinity).round(5)
+    end
 
-  def test_nominal
-    rate = Rate.new(0.0375, :effective)
-    assert_equal D('0.03687'), rate.nominal.round(5)
+    should "raise an exception if an unknown value is given for :type" do
+      assert_raises(ArgumentError){ Rate.new(0.0375, :foo) }
+    end
   end
 end
