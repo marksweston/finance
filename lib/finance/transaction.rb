6c7a68b1dae3229c32329ad6875f2baa5ba4c34f
William Kranec <wkranec@gmail.com>
2011-07-16 12:30:31 +0300

    Implemented Finance namespace; refactored to use decimal.rb.

--- lib/finance/transaction.rb
+++ lib/finance/transaction.rb
@@ -1,46 +1,52 @@
-# the Transaction class provides an interface for individual cash flows.
-# @api public
-class Transaction
-  # @return [DecNum] the cash value of the transaction
-  # @api public
-  attr_accessor :amount
+require_relative 'decimal'
 
-  # @return [DecNum] the difference between the 
+module Finance
+  # the Transaction class provides an interface for individual cash flows.
   # @api public
-  def additional_amount
-    @amount - @original
-  end
+  class Transaction
+    # @return [DecNum] the cash value of the transaction
+    # @api public
+    attr_accessor :amount
 
-  # create a new Transaction
-  # @return [Transaction]
-  # @param [Numeric] amount the cash value of the transaction
-  def initialize(amount)
-    @amount = amount
-    @original = amount
-  end
-
-  # @api public
-  def inspect
-    "Payment(#{@amount})"
-  end
-
-  # @return none
-  # @param [Block] modifier a block which returns a modified amount for the transaction
-  # @api public
-  def modify(&modifier)
-    value = modifier.call(self)
-    
-    # There's a chance that the block does not return a decimal.
-    unless value.class == Flt::DecNum
-      value = Flt::DecNum value.to_s
+    # @return [DecNum] the difference between the 
+    # @api public
+    def additional_amount
+      @amount - @original
     end
 
-    @amount = value
-  end
+    # create a new Transaction
+    # @return [Transaction]
+    # @param [Numeric] amount the cash value of the transaction
+    def initialize(amount, type=nil)
+      @amount = amount
+      @original = amount
+      @type = type
+    end
 
-  # (see #amount)
-  # @depreciated Provided for backwards compatibility
-  def payment
-    @amount
+    def interest?
+      @type.eql? :interest
+    end
+
+    # @api public
+    def inspect
+      "Payment(#{@amount})"
+    end
+
+    # @return none
+    # @param [Block] modifier a block which returns a modified amount for the transaction
+    # @api public
+    def modify(&modifier)
+      @amount = modifier.call(self).to_decimal
+    end
+
+    # (see #amount)
+    # @depreciated Provided for backwards compatibility
+    def payment
+      @amount
+    end
+
+    def payment?
+      @type.eql? :payment
+    end
   end
 end
