23dbd23cf82b54ad1b39bf1abaeeaf8de30fd241
William Kranec <wkranec@gmail.com>
2011-07-16 12:33:15 +0300

    Implemented Finance namespace; removed nper function (YAGNIL).

--- lib/finance.rb
+++ lib/finance.rb
@@ -12,10 +12,4 @@ module Finance
   autoload :Amortization, 'finance/amortization'
   autoload :Rate,         'finance/rates'
   autoload :Transaction,  'finance/transaction'
-
-  # Return the number of periods needed to pay off a loan with the
-  # given payment.
-  def Finance.nper(payment, rate, principal)
-    -(Math.log(1-((principal/payment)*rate))) / Math.log(1+rate)
-  end
 end
