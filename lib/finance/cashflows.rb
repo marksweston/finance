7dd6772c78985b827eb50c848b32c8a3ec94dba9
William Kranec <wkranec@gmail.com>
2011-07-20 10:06:21 +0300

    Refactored methods for readability and DecNum outputs; some documentation fixes.

--- lib/finance/cashflows.rb
+++ lib/finance/cashflows.rb
@@ -1,36 +1,45 @@
+require_relative 'decimal'
+
 module Finance
+  # Provides methods for working with cash flows (collections of transactions)
+  # @api public
   module Cashflow
     # calculate the internal rate of return for a sequence of cash flows
-    # @return [Numeric] the internal rate of return
+    # @return [DecNum] the internal rate of return
     # @example
     #   [-4000,1200,1410,1875,1050].irr #=> 0.143
     # @see http://en.wikipedia.org/wiki/Internal_rate_of_return
     # @api public
     def irr(iterations=100)
-      rate = 1.0
-      investment = self[0]
-      for i in 1..iterations+1
-        rate = rate * (1 - self.npv(rate) / investment)
+      self.collect! { |entry| entry.to_d }
+
+      rate, investment = 1.to_d, self[0]
+      iterations.times do
+        rate *= (1 - self.npv(rate) / investment)
       end
+      
       rate
     end
 
     # calculate the net present value of a sequence of cash flows
-    # @return [Numeric] the net present value
+    # @return [DecNum] the net present value
     # @param [Numeric] rate the discount rate to be applied
     # @example
     #   [-100.0, 60, 60, 60].npv(0.1) #=> 49.211
     # @see http://en.wikipedia.org/wiki/Net_present_value
     # @api public
     def npv(rate)
-      total = 0.0
+      self.collect! { |entry| entry.to_d }
+
+      rate, total = rate.to_d, 0.to_d
       self.each_with_index do |cashflow, index|
-        total = total + cashflow / (1+rate) ** index
+        total += cashflow / (1 + rate) ** index
       end
+
       total
     end
 
-    # @return [Numeric] the sum
+    # @return [Numeric] the total value of a sequence of cash flows
     # @api public
     def sum
       self.inject(:+)
