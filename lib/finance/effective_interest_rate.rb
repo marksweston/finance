279e161e5a50dfbc1365fde2b04180e38d845b9b
wiseleyb <wiseleyb@gamil.com>
2012-09-10 10:14:54 +1000

    comments

--- lib/finance/effective_interest_rate.rb
+++ lib/finance/effective_interest_rate.rb
@@ -10,6 +10,7 @@ module Finance
     FINANCIAL_MAX_ITERATIONS = 128
 
 
+    # Implementation of Execl's RATE function
     # RATE  RATE(NPER, PMT, PV, FV, type, guess)  
     # Returns the constant interest rate per period of an annuity. 
     # => NPER is the total number of periods, during which payments are made (payment period). 
@@ -18,6 +19,10 @@ module Finance
     # => FV (optional) is the future value, which is reached at the end of the periodic payments. 
     # => Type (optional) defines whether the payment is due at the beginning (1) or the end (0) of a period. 
     # => Guess (optional) determines the estimated value of the interest with iterative calculation.
+    # see test/test_effective_interest_rate.rb for examples
+    # adapted from Java code found here: http://www.pcpros.com/software/programming/java/java.shtml
+    # Example:
+    # Finance::EffectiveInterestRate.calc_effective_interest_rate(360, -1433.39, 200000)
     def self.calc_effective_interest_rate(nper, pmt, pv, fv = 0.0, type = 0, guess = 0.1) 
       
       y = 0; y0 = 0; y1 = 0; x0 = 0; x1 = 0; f = 0; i = 0
@@ -64,6 +69,9 @@ module Finance
       return rate * 100
     end
 
+    # This will return identical results that Finance::Amortization will given annual_fee = 0
+    # Simply adds ability to use annual_fee
+    # see test/test_effective_interest_rate.rb for examples
     def self.calc_payment(principal, rate, periods, annual_fee = 0)
       -(rate/12)*((annual_fee*periods/12)+principal)/(1-(1+rate/12) ** -periods)
     end
