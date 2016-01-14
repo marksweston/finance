a4f7ca1341752140d4beebc449ba659c24afb7dd
weshatheleopard <github@wesha.name>
2016-01-14 00:26:09 +0000

    Making sure the DST doesn't mess up with interest calcilations

--- test/test_cashflows.rb
+++ test/test_cashflows.rb
@@ -30,9 +30,11 @@ describe "Cashflows" do
     end
 
     it "should properly calculate IRR when Complex numbers arise from calculations" do
-      arr = [ Finance::Transaction.new( 70, :date => Time.new(2015,7,31)),
-              Finance::Transaction.new(-90, :date => Time.new(2021,1,13)),
-              Finance::Transaction.new(-20, :date => Time.new(2021,3,31)) ]
+      # Note that times must be in UTC (+00:00) here, so
+      # the local system's DST settings don't screw us up.
+      arr = [ Finance::Transaction.new( 70, :date => Time.new(2015,7,31,0,0,0,'+00:00')),
+              Finance::Transaction.new(-90, :date => Time.new(2021,1,13,0,0,0,'+00:00')),
+              Finance::Transaction.new(-20, :date => Time.new(2021,3,31,0,0,0,'+00:00')) ]
       assert_equal D("0.085677"), arr.xirr.effective.round(6)
     end
 
