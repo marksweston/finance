9036eedb39954749c013b03fe34405d2f31ccc34
weshatheleopard <github@wesha.name>
2016-01-19 19:14:16 +0000

    Update another test. This is not critical, but let's be consistent

--- test/test_cashflows.rb
+++ test/test_cashflows.rb
@@ -32,9 +32,9 @@ describe "Cashflows" do
     it "should properly calculate IRR when Complex numbers arise from calculations" do
       # Note that times must be in UTC (+00:00) here, so
       # the local system's DST settings don't screw us up.
-      arr = [ Finance::Transaction.new( 70, :date => Time.new(2015,7,31,0,0,0,'+00:00')),
-              Finance::Transaction.new(-90, :date => Time.new(2021,1,13,0,0,0,'+00:00')),
-              Finance::Transaction.new(-20, :date => Time.new(2021,3,31,0,0,0,'+00:00')) ]
+      arr = [ Finance::Transaction.new( 70, :date => Time.new(2015, 7, 31, 0, 0, 0,'+00:00')),
+              Finance::Transaction.new(-90, :date => Time.new(2021, 1, 13, 0, 0, 0,'+00:00')),
+              Finance::Transaction.new(-20, :date => Time.new(2021, 3, 31, 0, 0, 0,'+00:00')) ]
       assert_equal D("0.085677"), arr.xirr.effective.round(6)
     end
 
@@ -42,9 +42,9 @@ describe "Cashflows" do
       prev_max = 99999
 
       (1..30).each do |n|
-        arr = [ Finance::Transaction.new(100, :date => Time.new(2001,1,1)),
-                Finance::Transaction.new(-50, :date => Time.new(2001,2,1)),
-                Finance::Transaction.new(-60, :date => Time.new(2001,2,1) + n * 30*24*3600) ]
+        arr = [ Finance::Transaction.new(100, :date => Time.new(2001, 1, 1, 0, 0, 0,'+00:00')),
+                Finance::Transaction.new(-50, :date => Time.new(2001, 2, 1, 0, 0, 0,'+00:00')),
+                Finance::Transaction.new(-60, :date => Time.new(2001, 2, 1, 0, 0, 0,'+00:00') + (n * 30*24*3600)) ]
 
         irr = arr.xirr.effective.round(6)
 
